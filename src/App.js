1136c4476f7225ff3e362391f5fedb5f75d7cd9e
AlexShom <a.shomalistos@gmail.com>
2019-12-19 11:54:59 +0000

    test to master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This code is a test of our pipeline.
         </p>
         <a
           className="App-link"
